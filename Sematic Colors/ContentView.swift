17f2baf19ab52d9095ca03058dd9a309a1ff7be3
Brian Chevalier <brianbadahdah@gmail.com>
2019-09-09 13:56:13 -0700

    Initial Commit

---
+++ Sematic Colors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sematic Colors
+//
+//  Created by Adam Badahdah on 9/9/19.
+//  Copyright © 2019 Brian Chevalier. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
